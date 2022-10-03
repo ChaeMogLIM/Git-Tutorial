f1394afc3926657d8e920a8d37a2d55a7349ff51
ChaeMogLIM <113702358+ChaeMogLIM@users.noreply.github.com>
2022-10-03 22:54:48 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	for (i = 0; i<=5; i++)
+	{
+		for (j = 5 - i; j > 0; j--)
+		{
+			printf(" ");
+		}
+		for (j = 0; j < i; j++)
+		{
+			printf("*");
+		}
+		for (j = 0; j < i - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
