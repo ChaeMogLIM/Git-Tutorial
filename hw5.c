137d73ccf086212b0f911586e7b31e81d0bc523a
ChaeMogLIM <113702358+ChaeMogLIM@users.noreply.github.com>
2022-10-19 00:43:39 +0900

    Add files via upload

---
+++ hw5.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void f(int n)
+{
+	if (n > 1)f(n > 2);
+	printf("%d", n % 2);
+}
+int main()
+{
+	int n;
+	printf("Please enter a number: ");
+	scanf("%d", &n);
+	printf("%d", n%2);
+	f(n);
+}
